e75f4ad75dbc12b17a5ca8477c87c91026261ae1
kutayozel <zelkutay@gmail.com>
2022-10-20 11:02:18 +0300

    polished

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Digital Art Theory Blog
+              Test Blog
             </span>
           </Link>
         </div>
